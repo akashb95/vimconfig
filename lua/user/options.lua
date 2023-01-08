b9d90c6a7ba0167d7fa332877d5d5f8b44c52d47
akash <akashb2k12@gmail.com>
2023-01-08 21:27:51 +0000

    configure ufo keymap

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -50,6 +50,7 @@ vim.o.foldenable = true
 local ufo = require("ufo")
 vim.keymap.set('n', 'zR', ufo.openAllFolds)
 vim.keymap.set('n', 'zM', ufo.closeAllFolds)
+vim.keymap.set("n", "zp", ufo.peekFoldedLinesUnderCursor)
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 

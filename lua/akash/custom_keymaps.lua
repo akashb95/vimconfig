3b3d7712387ce0dfe4d0b8728408d97cddd54320
akash <akash@thoughtmachine.net>
2024-05-03 16:21:43 +0100

    Remove keymaps which don't work on Linux

--- lua/akash/custom_keymaps.lua
+++ lua/akash/custom_keymaps.lua
@@ -1,7 +1,3 @@
--- Linebreaks
-vim.keymap.set('n', '<C-CR>', 'i<cr><esc>', {})
-vim.keymap.set('n', '<S-CR>', 'O<esc>', {})
-
 -- Insert-mode navigation
 vim.keymap.set("i", "jj", "<esc>")
 vim.keymap.set("i", "II", "<esc>I")

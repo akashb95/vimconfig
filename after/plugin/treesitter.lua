ed8073b4320009df8e605b42e8aed2afbc8d34d0
akash <akash@thoughtmachine.net>
2024-05-03 16:20:19 +0100

    Add valid swap targets for treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -31,6 +31,7 @@ require("nvim-treesitter.configs").setup({
       node_decremental = "N",
     },
   },
+  indent = { enable = true },
   -- All identifiers with the same name are highlighted in the same colour.
   markid = {
     enable = true,
@@ -53,12 +54,12 @@ require("nvim-treesitter.configs").setup({
         ["[l"] = "@loop.outer",
       },
       goto_next_end = {
-        ["]F"] = "@function.outer",
-        ["]L"] = "@loop.outer",
+        [")f"] = "@function.outer",
+        [")l"] = "@loop.outer",
       },
       goto_previous_end = {
-        ["[F"] = "@function.outer",
-        ["[L"] = "@loop.outer",
+        ["(f"] = "@function.outer",
+        ["(l"] = "@loop.outer",
       },
     },
     select = {
@@ -79,11 +80,29 @@ require("nvim-treesitter.configs").setup({
     swap = {
       enable = true,
       swap_next = {
-        ["<leader>;"] = "@swappable",
+        ["<leader>nip"] = "@parameter.inner",
+        ["<leader>naf"] = "@function.outer",
+        ["<leader>naC"] = "@class.outer",
+        ["<leader>nil"] = "@literal_value.inner",
       },
       swap_previous = {
-        ["<leader>,"] = "@swappable",
+        ["<leader>Nip"] = "@parameter.inner",
+        ["<leader>Naf"] = "@function.outer",
+        ["<leader>NaC"] = "@class.outer",
+        ["<leader>Nil"] = "@literal_value.inner",
       },
     },
   },
 })
+
+local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+-- vim way: ; goes to the direction you were moving.
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+
+-- make builtin f, F, t, T also repeatable with ; and ,
+vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)

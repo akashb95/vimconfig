dd20bcdcab70ce6ad610f1423dc932dbb6549c15
akash <akashb2k12@gmail.com>
2025-09-21 11:13:26 +0100

    Stop using nvim-lspconfig and use vim.lsp instead (attempt 3)

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,6 +1,3 @@
-local lspconfig = require("lspconfig")
-local util = require("lspconfig.util")
-
 -- Enable the following language servers
 --  Add any additional override configuration in the following tables. Available keys are:
 --  - cmd (table): Override the default command used to start the server
@@ -8,6 +5,7 @@ local util = require("lspconfig.util")
 --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
 --  - settings (table): Override the default settings passed when initializing the server.
 --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+--  - root_dir (function): Dynamically determine whether to enable LSP given the buffer number.
 local servers = {
 	yamlls = {},
 	vimls = {},
@@ -16,13 +14,9 @@ local servers = {
 	buf_ls = {},
 	curlylint = {},
 	gopls = {
+		cmd = { "gopls" },
 		settings = {
 			gopls = {
-				diagnosticsDelay = "2s",
-				directoryFilters = { "-plz-out" },
-				linksInHover = false,
-				-- usePlaceholders = false,
-				-- semanticTokens = true,
 				codelenses = {
 					gc_details = true,
 					test = true,
@@ -31,24 +25,40 @@ local servers = {
 					regenerate_cgo = true,
 					upgrade_dependency = true,
 				},
+				diagnosticsDelay = "2s",
+				directoryFilters = { "-plz-out" },
+				linksInHover = false,
 			},
 		},
 		-- Must be set explicitly. Otherwise breaks on :LspRestart
 		single_file = false,
-		---@param startpath string
-		root_dir = function(startpath)
-			if string.find(startpath, "plz%-out") then
+		---@param buffer integer
+		-- @param on_dir function(root_dir?:string)
+		root_dir = function(buffer, on_dir)
+			local buffer_name = vim.api.nvim_buf_get_name(buffer)
+
+			local root
+
+			-- plz-out is where Please stores its artifacts.
+			if string.find(buffer_name, "plz%-out") then
 				-- Separate branch, because otherwise it defaults to the repo root and becomes too slow
-				return require("lspconfig/util").root_pattern("go.mod", "go.work")(startpath)
-			else
-				return require("lspconfig/util").root_pattern(
-					-- Order here matters
-					"BUILD",
-					"go.work",
-					"go.mod",
-					".git"
-				)(startpath)
+				root = vim.fs.dirname(vim.fs.find({ "go.mod", "go.work" }, { upward = true, type = "file" })[1])
 			end
+
+			root = vim.fs.dirname(vim.fs.find({
+				-- Order here matters
+				"BUILD",
+				"go.work",
+				"go.mod",
+				".git",
+			}, { upward = true, type = "file" })[1])
+
+			if not root then
+				-- Default to cwd.
+				root = vim.fs.dirname(buffer_name)
+			end
+
+			on_dir(root)
 		end,
 	},
 	lua_ls = {
@@ -71,29 +81,17 @@ local servers = {
 					verboseOutput = true,
 					logLevel = "Trace",
 					extraPaths = {
-						"plz_out/gen",
 						"plz-out/gen",
 						"plz-out/python/venv",
 					},
+					exclude = { "plz-out" },
 				},
 			},
 		},
-		on_new_config = function(config, root_dir)
-			if util.root_pattern(".plzconfig") then
-				config.settings = vim.tbl_deep_extend("force", config.settings, {
-					python = {
-						analysis = {
-							extraPaths = {
-								vim.fs.joinpath(root_dir, "plz-out/python/venv"),
-							},
-							exclude = { "plz-out" },
-						},
-					},
-				})
-			end
-		end,
-		root_dir = function()
-			return vim.fn.getcwd()
+		---@param buffer integer
+		-- @param on_dir function(root_dir?:string)
+		root_dir = function(buffer, on_dir)
+			on_dir(vim.fs.dirname(vim.api.nvim_buf_get_name(buffer)))
 		end,
 	},
 	rust_analyzer = {},
@@ -110,22 +108,16 @@ require("mason").setup()
 
 -- You can add other tools here that you want Mason to install
 -- for you, so that they are available from within Neovim.
-local ensure_installed = vim.tbl_keys(servers or {})
-vim.list_extend(ensure_installed, {
+vim.list_extend(vim.tbl_keys(servers or {}), {
 	"stylua", -- Used to format lua code
 	{ "black", version = "23.7.0" },
 	"pylint",
 	-- "goimports",
 	"prettier",
-	"rustfmt",
 })
-require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+require("mason-tool-installer").setup({ ensure_installed = vim.tbl_keys(servers or {}) })
 
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = vim.tbl_deep_extend("force", capabilities, vim.lsp.protocol.make_client_capabilities())
-
-local function extend_capabilities_and_setup(server_name)
-	local server = servers[server_name] or {}
+local function extend_capabilities_and_setup(server_name, server_config)
 	-- LSP servers and clients are able to communicate to each other what features they support.
 	--  By default, Neovim doesn't support everything that is in the LSP Specification.
 	--  When you add blink-cmp, luasnip, etc. Neovim now has *more* capabilities.
@@ -133,34 +125,48 @@ local function extend_capabilities_and_setup(server_name)
 	-- This also handles overriding only values explicitly passed
 	-- by the server configuration above. Useful when disabling
 	-- certain features of an LSP (for example, turning off formatting for tsserver)
-	server.capabilities = vim.tbl_deep_extend(
-		"force",
-		{},
-		require("blink.cmp").get_lsp_capabilities(server.capabilities),
-		server.capabilities or {}
-	)
-	lspconfig[server_name].setup(server)
+	server_config.capabilities = vim.tbl_deep_extend("force", {
+		-- Some LSPs need to be told line-folding is allowed (e.g. yamlls).
+		-- No harm in letting all LSPs know.
+		capabilities = {
+			textDocument = {
+				foldingRange = {
+					dynamicRegistration = false,
+					lineFoldingOnly = true,
+				},
+			},
+		},
+	}, require("blink.cmp").get_lsp_capabilities(server_config.capabilities), server_config.capabilities or {})
+
+	vim.lsp.enable(server_name, vim.lsp.config(server_name, server_config))
 end
 
--- mason-lspconfig.nvim will automatically enable (vim.lsp.enable()) installed servers for you by default.
-require("mason-lspconfig").setup({
-	automatic_enable = {
-		exclude = {
-			-- For some reason, setting gopls up via mason-lspconfig doesn't work. Setup gopls manually.
-			"gopls",
-		},
-	},
-	handlers = { extend_capabilities_and_setup },
-})
-extend_capabilities_and_setup("gopls")
+for server_name, server_config in pairs(servers) do
+	extend_capabilities_and_setup(server_name, server_config)
+end
 
--- Defines and sets up the the please language server (this is the only one that is not inlcuded in lspconfig.configs by
--- default, and is also not included in mason)
-require("lspconfig.configs").please = {
-	default_config = {
+-- Configure Please LSP separately so that Mason doesn't try to install it.
+vim.lsp.enable(
+	"please",
+	vim.lsp.config("please", {
 		cmd = { "plz", "tool", "lps" },
 		filetypes = { "please" },
-		root_dir = util.root_pattern(".plzconfig"),
-	},
-}
-require("lspconfig").please.setup({})
+		---@param buffer integer
+		-- @param on_dir function(root_dir?:string)
+		root_dir = function(_, on_dir)
+			on_dir(vim.fs.dirname(vim.fs.find({ ".plzconfig" }, { upward = true, type = "file" })[1]))
+		end,
+	})
+)
+
+-- TODO: delete after confirming vim.lsp config works
+-- Defines and sets up the the please language server (this is the only one that is not inlcuded in lspconfig.configs by
+-- default, and is also not included in mason)
+-- require("lspconfig.configs").please = {
+-- 	default_config = {
+-- 		cmd = { "plz", "tool", "lps" },
+-- 		filetypes = { "please" },
+-- 		root_dir = util.root_pattern(".plzconfig"),
+-- 	},
+-- }
+-- require("lspconfig").please.setup({})
--- init.lua
+++ init.lua
@@ -2,14 +2,3 @@ require("opts")
 require("custom_keymaps")
 
 require("lazyconfig")
-
-vim.lsp.config("*", {
-	capabilities = {
-		textDocument = {
-			foldingRange = {
-				dynamicRegistration = false,
-				lineFoldingOnly = true,
-			},
-		},
-	},
-})

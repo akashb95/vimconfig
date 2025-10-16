d5f162b3901ac52674064c791d5b97cb989a859f
Akash Bhattacharya <akash@thoughtmachine.net>
2025-10-16 17:17:06 +0100

    LSP shortcut to toggle diagnostic float

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,16 +1,10 @@
 -- vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "gd", function()
-	vim.lsp.buf.definition({ reuse_win = true })
+	vim.lsp.buf.definition({ reuse_win = false })
 end, { desc = "[g]o to [d]eclaration", noremap = true })
--- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 -- vim.keymap.set("n", "go", vim.lsp.buf.type_definition, {})
 vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
--- vim.keymap.set("n", "<leader>ge", vim.diagnostic.goto_next, {})
--- vim.keymap.set("n", "<leader>gp", vim.diagnostic.goto_prev, {})
--- -- Map find references to telescope picker
--- vim.keymap.set("n", "grr", function()
--- 	require("telescope.builtin").lsp_references({
--- 		show_line = true,
--- 	})
--- end, {})
 vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "[g]o to diagnostic f[l]oat" })
+vim.keymap.set("n", "gtl", function()
+	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, { desc = "[g]o [t]oggle diagnostic f[l]oat" })

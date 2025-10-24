954544c6f7539a24c64c63419a0d8a73951ddbd4
Akash Bhattacharya <akash@thoughtmachine.net>
2025-10-24 15:21:05 +0100

    Adjust diagnostic float and Telescope/Git keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,15 +1,15 @@
 -- vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "gd", function()
-	vim.lsp.buf.definition({ reuse_win = false })
+	vim.lsp.buf.definition({ reuse_win = true })
 end, { desc = "[g]o to [d]eclaration", noremap = true })
 -- vim.keymap.set("n", "go", vim.lsp.buf.type_definition, {})
 vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "[g]o to diagnostic f[l]oat" })
-vim.keymap.set("n", "gtl", function()
+vim.keymap.set("n", "df", vim.diagnostic.open_float, { desc = "open [d]iagnostic f[l]oat", noremap = true })
+vim.keymap.set("n", "tdf", function()
 	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
 	if vim.diagnostic.is_enabled() then
 		vim.notify("Showing inline diagnostics")
 	else
 		vim.notify("Hiding inline diagnostics")
 	end
-end, { desc = "[g]o [t]oggle diagnostic f[l]oat" })
+end, { desc = " [t]oggle [d]iagnostic [f]loat" })

d427aede29da5005adbb33291f3e3bc3a44fd4e3
akashb95 <akashb2k12@gmail.com>
2025-11-28 14:44:33 +0100

    ftplugin updates

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,3 +1,3 @@
-vim.opt_local.textwidth = 100
-vim.opt_local.colorcolumn = "100"
+vim.opt_local.textwidth = 120
+vim.opt_local.colorcolumn = "120"
 vim.opt_local.formatoptions:remove("t")
--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -10,5 +10,38 @@ vim.keymap.set(
 	function()
 		vim.cmd.RustLsp({ "hover", "actions" })
 	end,
-	{ silent = true, buffer = bufnr, desc = "Hover" }
+	{ silent = true, buffer = bufnr, desc = "Rustacean Hover" }
+)
+
+vim.keymap.set(
+  {"n", "v"},
+  "gK",
+  function ()
+    vim.cmd.RustLsp("openDocs")
+  end,
+  { silent = true, buffer = bufnr, desc = "[G]o to Rust do[K]s" }
+)
+
+vim.keymap.set(
+  "n",
+  "J",
+  function ()
+    vim.cmd.RustLsp('joinLines')
+  end,
+  { silent = true, buffer = bufnr, desc = "[J]oin lines" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>ftr",
+  function ()
+    vim.cmd.RustLsp {"flyCheck", "run"}
+  end
+)
+vim.keymap.set(
+  "n",
+  "<leader>ftc",
+  function ()
+    vim.cmd.RustLsp {"flyCheck", "clear"}
+  end
 )

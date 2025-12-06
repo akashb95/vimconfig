c3f01630329be8e2da4acc9ba5a9443bf7931756
akash <akashb2k12@gmail.com>
2025-12-06 16:14:48 +0100

    More CodeCompanion changes

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -5,7 +5,8 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 	},
 	config = function()
-		require("codecompanion").setup({
+		local codecompanion = require("codecompanion")
+		codecompanion.setup({
 			strategies = {
 				chat = { adapter = "gemini" },
 				inline = { adapter = "gemini" },
@@ -15,14 +16,31 @@ return {
 				http = {
 					gemini = function()
 						return require("codecompanion.adapters").extend("gemini", {
-							-- schema = {
-							-- 	model = {
-							-- 		default = "gemini-3-pro-preview",
-							-- 	},
-							-- },
+							schema = {
+								model = {
+									default = "gemini-3-pro-preview",
+								},
+							},
 							env = {
 								-- 1. Execute the 1Password CLI command
 								-- 2. vim.trim() removes the trailing newline character
+								api_key = vim.trim(vim.fn.system({
+									"op",
+									"read",
+									"op://Employee/ukafu5czkq37oo7thictjenh4e/credential",
+								})),
+							},
+						})
+					end,
+				},
+				acp = {
+					gemini_cli = function()
+						return require("codecompanion.adapters").extend("gemini_cli", {
+							defaults = {
+								auth_method = "gemini-api-key", -- Or "oauth-personal" | "vertex-ai"
+							},
+							env = {
+								-- Reusing your existing 1Password CLI setup for the API key
 								api_key = vim.trim(vim.fn.system({
 									"op",
 									"read",
@@ -46,5 +64,29 @@ return {
 			"<cmd>CodeCompanionChat Toggle<cr>",
 			{ desc = "[c]odecompanion [c]hat [t]oggle", noremap = true, silent = true }
 		)
+
+		codecompanion.setup({
+			strategies = {
+				inline = {
+					keymaps = {
+						accept_change = {
+							modes = { n = "ga" },
+							description = "Accept the suggested change",
+						},
+						reject_change = {
+							modes = { n = "gr" },
+							opts = { nowait = true },
+							description = "Reject the suggested change",
+						},
+						stop = {
+							modes = { n = "q" },
+							index = 4,
+							callback = "keymaps.stop",
+							description = "Stop (abort) request",
+						},
+					},
+				},
+			},
+		})
 	end,
 }

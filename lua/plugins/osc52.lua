be8860013702349be1220537c14adf10c56f1dc3
akashb95 <akashb2k12@gmail.com>
2025-12-08 11:04:34 +0100

    Copy to system clipboard

--- lua/plugins/osc52.lua
+++ lua/plugins/osc52.lua
@@ -1,6 +1,20 @@
 return {
 	"ojroques/nvim-osc52",
-	opts = {
-		trim = true,
-	},
+  lazy = false,
+  config = function()
+    require('osc52').setup {
+      max_length = 0,           -- Maximum length of selection (0 for no limit)
+      silent = false,           -- Disable message on successful copy
+      trim = false,             -- Trim surrounding whitespaces before copy
+    }
+
+    -- Automatically copy to system clipboard on yank
+    local function copy()
+        if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
+            require('osc52').copy_register('+')
+        end
+    end
+
+    vim.api.nvim_create_autocmd('TextYankPost', {callback = copy})
+  end,
 }

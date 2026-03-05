ac737cf18a55775255b6b87b3c347c6f4127448a
akashb95 <akashb2k12@gmail.com>
2026-03-05 13:36:59 +0100

    Fix rust-analyzer root-finding logic

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -8,21 +8,13 @@ return {
 			server = {
 				-- Configure the root_dir to find the nearest Cargo.toml or .git directory
 				root_dir = function(filename, default_root_dir_func)
-					local current_buffer_dir = vim.fs.dirname(filename)
-					local found_root = vim.fs.find({ "Cargo.toml", ".git" }, {
-						upward = true,
-						path = current_buffer_dir,
-						type = "file",
-					})
-
-					if #found_root > 0 then
-						-- Return the directory of the found file/directory
-						return vim.fs.dirname(found_root[1])
-					else
-						-- Fallback to the default root_dir calculation if nothing is found
-						return default_root_dir_func(filename)
+					local root = vim.fs.root(filename, { "Cargo.toml", ".git" })
+					if root then
+						return root
 					end
+					return default_root_dir_func(filename)
 				end,
+
 				auto_attach = function(bufnr)
 					if #vim.bo[bufnr].buftype > 0 then
 						return false
@@ -34,6 +26,7 @@ return {
 
 					return true
 				end,
+
         default_settings = {
           --- options to send to rust-analyzer
           --- See: https://rust-analyzer.github.io/book/configuration

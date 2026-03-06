e46657e30d12b6cb71594114ce31a1df417f901a
akash <akashb2k12@gmail.com>
2026-03-06 22:43:36 +0100

    Make git-blame Lazy load and tidy up keymaps

--- lua/plugins/gitblame.lua
+++ lua/plugins/gitblame.lua
@@ -1,22 +1,35 @@
 return {
 	"f-person/git-blame.nvim",
 
-	lazy = false,
+	event = { "BufReadPost", "BufNewFile" },
 
 	keys = {
-		{ "<leader>gburl", "<CMD>GitBlameOpenCommitURL<CR>", desc = "[g]it [b]lame open commit [url]" },
-	},
-	keys = {
-		{ "<leader>gbcurl", "<CMD>GitBlameCopyCommitURL<CR>", desc = "[g]it [b]lame [c]opy commit [url]" },
+		{
+			"<leader>gbt",
+			"<CMD>GitBlameToggle<CR>",
+			desc = "[g]it [b]lame [t]oggle",
+		},
+		{
+			"<leader>gbocurl" --[[  ]],
+			"<CMD>GitBlameOpenCommitURL<CR>",
+			desc = "[g]it [b]lame [o]pen [c]ommit [url]",
+		},
+		{
+			"<leader>gbcurl",
+			"<CMD>[GitBlameCopyCommitURL<CR>",
+			desc = "[g]it [b]lame [c]opy commit [url]",
+		},
 	},
 
 	opts = {
 		enabled = true,
-		message_template = " <date> • <author> • <summary>", -- template for the blame message
-
-		date_format = "%r", -- template for the date
+		message_template = "<date> • <author> • <summary>",
+		date_format = "%r",
 		max_commit_summary_length = 50,
-
 		virtual_text_column = 1,
+
+		delay = 250,
+
+		ignored_filetypes = { "help", "NvimTree" },
 	},
 }

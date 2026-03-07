baf5d54b0fb3658d9b49280ed56b781f560f3413
akash <akashb2k12@gmail.com>
2026-03-07 20:09:27 +0100

    Install sqlite for smart-history

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,14 +4,16 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			-- `build` is used to run some command when the plugin is installed/updated.
-			-- This is only run then, not every time Neovim starts up.
-			build = "make",
+		        "nvim-telescope/telescope-fzf-native.nvim",
+		        -- `build` is used to run some command when the plugin is installed/updated.
+		        -- This is only run then, not every time Neovim starts up.
+		        build = "make",
 		},
 		{
-			"nvim-telescope/telescope-live-grep-args.nvim",
-			version = "1.1.0",
+		        "kkharji/sqlite.lua",
+		},
+		{
+		        "nvim-telescope/telescope-live-grep-args.nvim",			version = "1.1.0",
 		},
 		{
 			"nvim-tree/nvim-web-devicons",

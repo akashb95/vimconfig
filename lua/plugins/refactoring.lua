0cb457c2356967d563405d9050e319859a0e2b85
akash <akashb2k12@gmail.com>
2026-05-14 20:39:59 +0100

    add refactoring select menu keymap

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -31,5 +31,10 @@ return {
 		vim.keymap.set({ "n", "x" }, "<leader>reb", function()
 			return refactoring.refactor("Extract Block")
 		end, { desc = "[r]efactor: [e]xtract [b]lock", expr = true })
+
+		vim.keymap.set({ "n", "x" }, "<leader>rs", function()
+			-- this keymap doesn't select any textobject by default, so you may need to provide one each time you use it.
+			refactoring.select_refactor()
+		end, { desc = "[r]efactor [s]elect" })
 	end,
 }

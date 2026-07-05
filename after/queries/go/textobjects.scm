b6d860b86bd79f2db1bcadcb2a07662527686c4a
Akash Bhattacharya <akash.bhattacharya@reveng.ai>
2026-07-05 10:56:51 +0100

    fix golang textobjects after treesitter update

--- after/queries/go/textobjects.scm
+++ after/queries/go/textobjects.scm
@@ -13,11 +13,9 @@
 ; slice values
 
 (literal_value
-  "," @_start .
-  (literal_element) @literal_value.inner
-  (#make-range! "literal_value.outer" @_start @literal_value.inner))
+  ","
+  (literal_element) @literal_value.inner)
 
 (literal_value
-  . (literal_element) @literal_value.inner
-  . ","? @_end
-  (#make-range! "literal_value.outer" @literal_value.inner @_end))
+  (literal_element) @literal_value.inner
+  ","?)

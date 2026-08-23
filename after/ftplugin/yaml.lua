833faf4cdbbe96a66b289d7bc85ccc45232314fc
Akash Bhattacharya <akash.bhattacharya@reveng.ai>
2026-08-23 14:17:46 +0100

    No tabs in YAML

--- after/ftplugin/yaml.lua
+++ after/ftplugin/yaml.lua
@@ -2,7 +2,5 @@ vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
 
-vim.opt_local.expandtab = false
-
 vim.opt_local.formatoptions:remove("t")
 vim.opt_local.formatoptions:remove("o")
